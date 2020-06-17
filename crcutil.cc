514068a4111267fcc0b62ca3d68284bf001a031a
Peter Bailey <peter.eldridge.bailey@gmail.com>
2020-06-17 00:17:44 -0700

    update note

--- crcutil.cc
+++ crcutil.cc
@@ -5,6 +5,9 @@ extern "C" uint32_t crc32(uint32_t prev, void  * data, size_t length);
 
 static constexpr uint32_t CRC_POLY = 0xEDB88320U;
 
+// Note: this implementation does not use CRC acceleration.
+// crcutil has an SSE4/CRC-accelerated implementation, but it implements CRC-32C
+// with a different polynomial than CRC_POLY.
 static crcutil::GenericCrc<uint32_t, uint32_t, size_t, 2> gCrc(CRC_POLY, 32, true);
 
 uint32_t crc32(uint32_t prev, void * data, size_t length){
